291e712051146d5c6bc64304fee0fa597106b1a7
peter-swe <lelkes@me.com>
2023-03-27 20:47:55 +0200

    added data.js

---
+++ constants/data.js
@@ -0,0 +1,22 @@
+const LM = [
+  {
+    id: "l1",
+    name: "LM-A",
+    styrka: "1 mg/ml",
+    beredning: "",
+    recept: "",
+    dosiMg: 0.1,
+    dosImikroG: "",
+    dosIMl: "",
+  },
+  {
+    id: "l2",
+    name: "Effedrin",
+    styrka: "5 mg/ml",
+    beredning: "",
+    recept: "1 ml Effedrin 50 mg/ml + 9 ml NaCl",
+    dosiMg: 0.1,
+    dosImikroG: "",
+    dosIMl: "",
+  },
+];
