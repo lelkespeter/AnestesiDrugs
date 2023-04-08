dd6ef1b2755ffba02ab01e17bf7f80387038c0cf
peter-swe <lelkes@me.com>
2023-04-08 12:36:07 +0200

    valuemodified

--- components/Input/WeightInput.js
+++ components/Input/WeightInput.js
@@ -15,7 +15,7 @@ const WeightInput = ({navigation}) => {
       <TextInput
         keyboardType="decimal-pad"
         placeholder="Weight i kg"
-        value={+enteredVikt}
+        value={enteredVikt}
         onChangeText={weightCangeHandler}
       />
 
